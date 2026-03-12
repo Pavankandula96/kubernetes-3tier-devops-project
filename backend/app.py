524aed05899d148b2721c81214b938c39fb7f87e
Venkata Pavan Kandula <pavankandula96@gmail.com>
2026-03-13 01:35:58 +0530

    files

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-import mysql.connector
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Flask backend running inside Kubernetes"
+
+@app.route("/health")
+def health():
+    return {"status":"ok"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
